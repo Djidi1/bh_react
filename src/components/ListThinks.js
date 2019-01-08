32bd50b18ea23dbd8ef1d1fbddbdb54aedcfc717
Daniil <daniil@rivc-pulkovo.ru>
2019-01-08 23:26:23 +0300

    add field

--- src/components/ListThinks.js
+++ src/components/ListThinks.js
@@ -9,6 +9,12 @@ import Checkbox from '@material-ui/core/Checkbox';
 import Swipeout from 'rc-swipeout';
 import 'rc-swipeout/assets/index.css';
 
+
+import IconButton from '@material-ui/core/IconButton';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Visibility from '@material-ui/icons/Visibility';
+import TextField from "@material-ui/core/TextField";
+
 const styles = theme => ({
     root: {
         width: '100%',
@@ -16,12 +22,17 @@ const styles = theme => ({
     },
     checkbox: {
         padding: 0,
-    }
+    },
+    textField: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+    },
 });
 
 class ListThinks extends React.Component {
     state = {
         checked: [0],
+        think: ''
     };
 
     handleToggle = value => () => {
@@ -44,39 +55,60 @@ class ListThinks extends React.Component {
         const { classes } = this.props;
 
         return (
-            <List
-                dense
-                disablePadding
-                className={classes.root}>
+            <div>
+                <TextField
+                    id="outlined-adornment-add"
+                    variant="outlined"
+                    type={ 'text' }
+                    label="Goods"
+                    value={''}
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="Add"
+                                    onClick={console.log('push')}
+                                >
+                                    <Visibility />
+                                </IconButton>
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+                <List
+                    dense
+                    disablePadding
+                    className={classes.root}>
 
-                {[0, 1, 2, 3].map(value => (
-                    <Swipeout
-                        key={value}
-                        right={[
-                            {
-                                text: 'delete',
-                                onPress:() => console.log('delete'),
-                                style: { backgroundColor: 'red', color: 'white' },
-                                className: 'custom-class-2'
-                            }
-                        ]}
-                        onOpen={() => console.log('open')}
-                        onClose={() => console.log('close')}
-                    >
-                        <div style={{height: 44}}>
-                            <ListItem role={undefined} dense button onClick={this.handleToggle(value)}>
-                                <Checkbox
-                                    className={classes.checkbox}
-                                    checked={this.state.checked.indexOf(value) !== -1}
-                                    tabIndex={-1}
-                                    disableRipple
-                                />
-                                <ListItemText primary={`Line item ${value + 1}`} />
-                            </ListItem>
-                        </div>
-                    </Swipeout>
-                ))}
-            </List>
+                    {[0, 1, 2, 3].map(value => (
+                        <Swipeout
+                            key={value}
+                            right={[
+                                {
+                                    text: 'delete',
+                                    onPress:() => console.log('delete'),
+                                    style: { backgroundColor: 'red', color: 'white' },
+                                    className: 'custom-class-2'
+                                }
+                            ]}
+                            onOpen={() => console.log('open')}
+                            onClose={() => console.log('close')}
+                        >
+                            <div style={{height: 44}}>
+                                <ListItem role={undefined} dense button onClick={this.handleToggle(value)}>
+                                    <Checkbox
+                                        className={classes.checkbox}
+                                        checked={this.state.checked.indexOf(value) !== -1}
+                                        tabIndex={-1}
+                                        disableRipple
+                                    />
+                                    <ListItemText primary={`Line item ${value + 1}`} />
+                                </ListItem>
+                            </div>
+                        </Swipeout>
+                    ))}
+                </List>
+            </div>
         );
     }
 }
